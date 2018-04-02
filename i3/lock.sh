16a6ae379fba4c0096c3eba07e8281137478204e
i3wgnit <i3wgnit@gmail.com>
2018-04-02 13:47:25 -0400

    Lock script now uses maim instead of scrot

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-scrot /tmp/screen.png
+maim /tmp/screen.png
 
 convert /tmp/screen.png -spread 12 -median 3 -paint 2 /tmp/screen.png
 [[ -f ~/.config/i3/lock.png ]] && convert /tmp/screen.png ~/.config/i3/lock.png -gravity center -composite -matte /tmp/screen.png
