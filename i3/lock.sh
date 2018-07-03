b3d25fc5fe17e7f53cf04aacf20dec54436e16ac
i3wgnit <i3wgnit@gmail.com>
2018-07-03 00:07:33 -0400

    Something something forgot to push?

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-maim /tmp/screen.png
+maim /tmp/screen.png -d 0.125
 
-convert /tmp/screen.png -spread 12 -median 3 -paint 2 /tmp/screen.png
-[[ -f ~/.config/i3/lock.png ]] && convert /tmp/screen.png ~/.config/i3/lock.png -gravity center -composite -matte /tmp/screen.png
+#convert /tmp/screen.png -spread 12 -median 3 -paint 2 /tmp/screen.png
+#convert /tmp/screen.png  -spread 3 -median 4 /tmp/screen.png
+[[ -f ~/.config/i3/lock.png ]] && \
+convert /tmp/screen.png -spread 3 -median 4 \
+~/.config/i3/lock.png -gravity center -composite -matte \
+/tmp/screen.png
 
-i3lock -uei /tmp/screen.png
+i3lock -fec 000000 -i /tmp/screen.png
 
---
+++ vim/vim/after/ftplugin/javascript.vim
@@ -0,0 +1,4 @@
+let g:ale_set_highlights = 0
+set tabstop=2
+set shiftwidth=2
+set softtabstop=2
