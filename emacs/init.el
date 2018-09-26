fe5ad777dd08331f4942bb55df590ddd22fd4f7f
i3wgnit <i3wgnit@gmail.com>
2018-09-26 19:28:44 -0400

    Random update

--- emacs/init.el
+++ emacs/init.el
@@ -26,11 +26,11 @@
  '(evil-collection-setup-minibuffer t)
  '(package-selected-packages
    (quote
-    (evil-magit magit iasm-mode racket-mode haskell-mode auctex-latexmk auctex htmlize ox-twbs org-bullets company diminish spaceline nlinum-relative pretty-mode dashboard evil-collection evil-indent-textobject evil-surround evil-leader evil flx golden-ratio fzf flycheck flyspell-correct-ivy counsel paredit avy beacon rainbow-delimiters which-key hungry-delete auto-compile use-package spacemacs-theme))))
+    (scala-mode fill-column-indicator evil-magit magit iasm-mode racket-mode haskell-mode auctex-latexmk auctex htmlize ox-twbs org-bullets company diminish spaceline nlinum-relative pretty-mode dashboard evil-collection evil-indent-textobject evil-surround evil-leader evil flx golden-ratio fzf flycheck flyspell-correct-ivy counsel paredit avy beacon rainbow-delimiters which-key hungry-delete auto-compile use-package spacemacs-theme))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "PfEd" :family "Iosevka")))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "PfEd" :family "Iosevka")))))
