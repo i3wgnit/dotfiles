fb7c5a194fe72315343ef8b6eb4e630c39ed7f64
i3wgnit <i3wgnit@gmail.com>
2019-01-30 20:04:05 -0500

    Use dwm instead of i3-wm

---
+++ dwm/autostart.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+~/.dotfiles/bin/wallpaper.sh
+
+setsid -f pulseaudio --start
+setsid -f emacs --daemon
+setsid -f rescuetime
+setsid -f redshift-gtk
+setsid -f dunst
+CM_SELECTIONS=clipboard setsid -f clipmenud
+# setsid -f compton -b
+# setsid -f qbittorrent-nox -d --webui-port=7890
+
+i3blocks | ~/.config/dwm/status.py &
+
