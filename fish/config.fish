a8a1e5993bab5c26d166ba8d944f1f787becb817
Ting Wei Liu <i3wgnit@gmail.com>
2020-03-14 19:04:15 -0400

    small fixes

--- fish/config.fish
+++ fish/config.fish
@@ -23,23 +23,23 @@ fish_vi_key_bindings ^ /dev/null
 #alias ....='cd ../../..'
 
 # c/c++
-alias gcc99="gcc -std=c99 -Wall"
-alias g++14="g++ -std=c++14 -Wall"
+abbr --add gcc99 'gcc -std=c99 -Wall'
+abbr --add g++14 'g++ -std=c++14 -Wall'
 
 # VIM
-alias v='vim'
+#alias v='vim'
 
 # Ranger
-alias ra='ranger'
+#abbr --add ra 'ranger'
 
 # Reload config.fish
-alias so='source ~/.config/fish/config.fish'
+abbr --add so 'source ~/.config/fish/config.fish'
 
 # Full system upgrade
-alias pSyyu='command yay --nouseask --noconfirm -Syyu'
+#alias pSyyu='command yay --nouseask --noconfirm -Syyu'
 
 # xclip
-alias xclip='xclip -selection c'
+#alias xclip='xclip -selection c'
 
 # latexmk
 #alias latexmks='grep -l \'\\documentclass\' *tex | xargs latexmk -f -pvc'
