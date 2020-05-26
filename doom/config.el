7a246c84917326b197c1b82eb9b28d91b3e9ee77
Ting Wei Liu <i3wgnit@gmail.com>
2020-05-26 03:54:17 -0400

    add more latex formatting in emacs

--- doom/config.el
+++ doom/config.el
@@ -91,4 +91,6 @@
             (LaTeX-newline))))))
   (advice-add #'LaTeX-fill-region-as-paragraph
               :around #'twl+latex//fill-sentence)
-  (add-to-list 'LaTeX-indent-environment-list '("algorithmic")))
+  (add-to-list 'LaTeX-indent-environment-list '("algorithmic"))
+  (add-to-list 'LaTeX-indent-environment-list '("asy"))
+  (add-to-list 'LaTeX-indent-environment-list '("asydef")))
