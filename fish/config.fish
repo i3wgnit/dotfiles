bfb786a1ca176185b1365131e98865b305c4e980
i3wgnit@gmail.com <i3wgnit@gmail.com>
2020-09-13 16:27:39 -0400

    other stuff

--- fish/config.fish
+++ fish/config.fish
@@ -6,18 +6,11 @@ if not test -f "$HOME/.config/fish/.setup"
         add_to_user_path /opt/local/bin /opt/local/sbin
     end
 
-    add_to_user_path "$HOME/.pyenv/bin" "$HOME/bin"
+    add_to_user_path "$HOME/src/pyenv/bin" "$HOME/bin" "$HOME/.local/bin"
     touch "$HOME/.config/fish/.setup"
 end
 
 # == Startup ==
-
-# Start X at login
-if status --is-login
-    if test -z "$DISPLAY" -a $XDG_VTNR = 1
-        exec startx -- -keeptty
-    end
-end
 
 tput smkx
 
@@ -31,18 +24,32 @@ and source "$HOME/.config/fish/config.local.fish"
 fish_vi_key_bindings ^ /dev/null
 
 # pyenv
-set -gx PYENV_ROOT "$HOME"/.pyenv
-
-# stuff
-set -gx LESSHISTFILE -
-set -gx DOCKER_CONFIG "$HOME"/.config/docker
-set -gx GNUPGHOME "$HOME"/.config/gnupg
-set -gx ASYMPTOTE_HOME "$HOME"/.config/asy
-
+set -gx PYENV_ROOT "$HOME"/src/pyenv
 if status --is-interactive; and command -v pyenv >/dev/null 2>&1
     pyenv init - | source
     pyenv virtualenv-init - | source
 end
+
+set -gx QT_QPA_PLATFORMTHEME 'qt5ct'
+
+# stuff
+set -gx XDG_CACHE_HOME "$HOME"/.cache
+set -gx XDG_CONFIG_HOME "$HOME"/.config
+set -gx XDG_DATA_HOME "$HOME"/.local/share
+
+set -gx ASYMPTOTE_HOME "$XDG_CONFIG_HOME"/asy
+set -gx DOCKER_CONFIG "$XDG_CONFIG_HOME"/docker
+set -gx GNUPGHOME "$XDG_CONFIG_HOME"/gnupg
+set -gx GTK2_RC_FILES "$XDG_CONFIG_HOME"/gtk-2.0/gtkrc
+set -gx LESSHISTFILE "$XDG_DATA_HOME"/less/history
+set -gx NODE_REPL_HISTORY "$XDG_DATA_HOME"/node_repl_history
+set -gx NUGET_PACKAGES "XDG_CACHE_HOME"/NuGetPackages
+set -gx PULSE_COOKIE "$XDG_CACHE_HOME"/pulse_cookie
+# set -gx WGETRC "$XDG_CONFIG_HOME"/wgetrc
+abbr --add wget 'env WGETRC="$XDG_CONFIG_HOME"/wgetrc wget --hsts-file="$XDG_CACHE_HOME"/wget-hsts'
+set -gx WINEPREFIX "$XDG_DATA_HOME"/wineprefixes/default
+set -gx XAUTHORITY "$XDG_RUNTIME_DIR"/Xauthority
+set -gx _JAVA_OPTIONS '-Djava.util.prefs.userRoot='"$XDG_CONFIG_HOME"/java
 
 
 # == Alias ==
@@ -53,3 +60,10 @@ abbr --add g++14 'g++ -std=c++14 -Wall'
 
 # Reload config.fish
 abbr --add so 'source ~/.config/fish/config.fish'
+
+# Start X at login
+if status --is-login
+    if test -z "$DISPLAY" -a (tty) = /dev/tty1
+        exec startx "$XDG_CONFIG_HOME"/X11/xinitrc
+    end
+end
