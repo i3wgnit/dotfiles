babf97179408ebc5c3dbcd9aeaa739d1d8fd64f6
i3wgnit@gmail.com <i3wgnit@gmail.com>
2020-09-13 22:13:56 -0400

    Customized doom configs

--- doom/config.el
+++ doom/config.el
@@ -103,15 +103,3 @@
 (setq LaTeX-paragraph-commands
       '("documentclass" "usepackage" "title" "author" "date" "vspace" "hspace"
         "problem" "subproblem" "subsubproblem"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(safe-local-variable-values (quote ((projectile-project-test-cmd . "./buildall.sh ")))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
--- doom/init.el
+++ doom/init.el
@@ -17,6 +17,7 @@
 (doom! :input
        ;;chinese
        ;;japanese
+       ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
        company               ; the ultimate code completion backend
@@ -28,17 +29,18 @@
        ;;deft            ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
-       doom-quit         ; DOOM quit-message prompts when you quit Emacs
+       ;;doom-quit         ; DOOM quit-message prompts when you quit Emacs
        fill-column       ; a `fill-column' indicator
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
        indent-guides     ; highlighted indent columns
+       ;;ligatures         ; ligatures and symbols to make your code pretty again
+       minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
        nav-flash         ; blink the current line after jumping
        ;;neotree         ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
        (popup +defaults) ; tame sudden yet inevitable temporary windows
-       ;;pretty-code     ; ligatures or substitute text with pretty symbols
        ;;tabs            ; an tab bar for Emacs
        ;;treemacs        ; a project drawer, like neotree but cooler
        unicode           ; extended unicode support for various languages
@@ -51,7 +53,7 @@
        :editor
        (evil +everywhere) ; come to the dark side, we have cookies
        file-templates     ; auto-snippets for empty files
-       fold               ; (nigh) universal code folding
+       ;;fold               ; (nigh) universal code folding
        ;;(format +onsave) ; automated prettiness
        ;;god              ; run Emacs commands without modifier keys
        ;;lispy            ; vim for lisp, for people who don't like vim
@@ -91,16 +93,20 @@
        ;;gist          ; interacting with github gists
        lookup          ; navigate your code and its documentation
        lsp
-       macos           ; MacOS-specific commands
        magit           ; a git porcelain for Emacs
-       ;;make          ; run make tasks from Emacs
+       make          ; run make tasks from Emacs
        ;;pass          ; password manager for nerds
        ;;pdf           ; pdf enhancements
        ;;prodigy       ; FIXME managing external services & code builders
        rgb             ; creating color strings
+       ;;taskrunner        ; taskrunner for all your projects
        ;;terraform     ; infrastructure as code
        tmux            ; an API for interacting with tmux
        upload        ; map local to remote projects via ssh/ftp
+
+       :os
+       (:if IS-MAC macos) ; improve compatibility with macOS
+       tty                ; improve the terminal Emacs experience
 
        :lang
        ;;agda              ; types of types of types of types...
@@ -140,7 +146,6 @@
        ;;nix               ; I hereby declare "nix geht mehr!"
        ocaml               ; an objective camel
        org                 ; organize your plain life in plain text
-       ;;perl              ; write code no one else can comprehend
        ;;php               ; perl's insecure younger brother
        plantuml            ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
