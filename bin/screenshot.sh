99090e762cd37cc9aa017c682185871dbf0fbf22
i3wgnit@gmail.com <i3wgnit@gmail.com>
2020-12-09 20:20:31 -0500

    fix screenshot.sh

--- bin/screenshot.sh
+++ bin/screenshot.sh
@@ -7,23 +7,26 @@ print_xclip() {
 MAIM_CMD='maim'
 
 SELECTION=${2:-"$(printf 'screen\nwindow\narea' | dmenu)"}
-[ -z "$selection" ] && exit
+[ -z "$SELECTION" ] && exit 1
 
 OUTPUT=${1:-"$(printf 'file\nclip' | dmenu)"}
-[ -z "$OUTPUT" ] && exit
+[ -z "$OUTPUT" ] && exit 1
 
 case "$OUTPUT" in
     file) case "$SELECTION" in
         screen) maim -qud 1 "$PRINT_FILE" ;;
         window) maim -ui "$(xdotool getactivewindow)" "$PRINT_FILE" ;;
         area) maim -us "$PRINT_FILE" ;;
+        *) exit 1 ;;
         esac;;
     clip) case "$SELECTION" in
         screen) maim -qud 1 | print_xclip ;;
         window) maim -ui "$(xdotool getactivewindow)" | print_xclip ;;
         area) maim -us | print_xclip ;;
+        *) exit 1 ;;
         esac;;
 esac
+
 
 notify-send \
     'Screenshot Captured' \
