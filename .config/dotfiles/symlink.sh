89a990cd3e1e8e58559a523027a15a04c3f75dca
i3wgnit@gmail.com <i3wgnit@gmail.com>
2020-12-28 19:52:14 -0500

    Update symlink script

--- .config/dotfiles/symlink.sh
+++ .config/dotfiles/symlink.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-ln -s ~/.config/bash/bashrc ~/.bashrc
-ln -s ~/.config/bash/bash_profile ~/.bash_profile
+link() {
+    if ln -s "$1" "$2" >/dev/null 2>&1 ; then
+        echo Created link "$1" '->' "$2"
+    else
+        echo Link exists "$1" '->' "$2"
+    fi
+}
 
-ln -s ~/.local/share/dotfiles/pyenv-virtualenv ~/.local/share/pyenv/plugins/
+link ~/.config/bash/bashrc ~/.bashrc
+link ~/.config/bash/bash_profile ~/.bash_profile
+
+link ~/.local/share/dotfiles/pyenv-virtualenv ~/.local/share/pyenv/plugins/
