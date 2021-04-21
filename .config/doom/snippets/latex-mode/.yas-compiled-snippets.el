f97659f86c03ff2e12cf55c38e45de830235af3c
i3wgnit@gmail.com <i3wgnit@gmail.com>
2021-04-20 20:38:03 -0400

    Update yas snippets

--- .config/doom/snippets/latex-mode/.yas-compiled-snippets.el
+++ .config/doom/snippets/latex-mode/.yas-compiled-snippets.el
@@ -8,7 +8,7 @@
                        ("newlecmain" "\\include{lectures/${1:`(format-time-string \"%Y-%m-%d\")`}.tex}\n$0" "newlecturemain" nil nil nil "/home/waigni/.config/doom/snippets/latex-mode/newlecturemain" nil nil)
                        ("lec" "%% -*- TeX-engine: xetex -*-\n\n\\documentclass[lecture]{twl}\n\n\\title{[${1:$$(upcase yas-text)}] ${2:Course Name}}\n\\author{${4:Ting Wei Liu}}\n\n\\begin{document}\n\\maketitle\n\n\\makeatletter\n\\RenewDocumentCommand{\\@title}{}{$1}\n\\makeatother\n\n\\begin{description}\n\\item[Professor] ${6:$$(capitalize yas-text)}${7:\n}\n\\end{description}\n\n\\tableofcontents\n\n$0\n\n\\PrintIndex{}\n\\PrintTheorems{}\n\\end{document}\n" "lecture" nil nil nil "/home/waigni/.config/doom/snippets/latex-mode/lecture" nil nil)
                        ("eg" "\\begin{example}\n`%`$0\n\\end{example}" "example" nil nil nil "/home/waigni/.config/doom/snippets/latex-mode/example" nil nil)
-                       ("asg" "%% -*- TeX-engine: xetex -*-\n\n\\documentclass[assignment]{twl}\n\n\\title{[${1:$$(upcase yas-text)}] ${2:Assignment} \\texttt{\\#}${3:Number}}\n\\author{${4:Ting Wei Liu}}\n\\date{${5:`(format-time-string \"%B %d, %Y\")`}}\n\n\\begin{document}\n\\maketitle\n\n\\tableofcontents\n\\clearpage\n\n\\problem{}\n\n$0\n\\end{document}" "assignment" nil nil nil "/home/waigni/.config/doom/snippets/latex-mode/assignment" nil nil)))
+                       ("asg" "%% -*- TeX-engine: xetex -*-\n\n\\documentclass[assignment]{twl}\n\n\\title{[${1:$$(upcase yas-text)}] ${2:Assignment} \\texttt{\\#}${3:Number}}\n\\author{${4:twl}}\n\\date{${5:`(format-time-string \"%B %d, %Y\")`}}\n\n\\begin{document}\n\\maketitle\n\n\\tableofcontents\n\\clearpage\n\n\\problem{}\n\n$0\n\\end{document}" "assignment" nil nil nil "/home/waigni/.config/doom/snippets/latex-mode/assignment" nil nil)))
 
 
-;;; Do not edit! File generated at Wed Apr 14 14:34:36 2021
+;;; Do not edit! File generated at Tue Apr 20 20:37:38 2021
