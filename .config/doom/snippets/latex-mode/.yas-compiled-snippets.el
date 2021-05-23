cec48c41029b550900ed5f7414a6ae364db42723
Ting Wei Liu <i3wgnit@protonmail.com>
2021-05-23 15:21:03 -0400

    Fix newlecturesub latex yasnippet

--- .config/doom/snippets/latex-mode/.yas-compiled-snippets.el
+++ .config/doom/snippets/latex-mode/.yas-compiled-snippets.el
@@ -4,11 +4,11 @@
 (yas-define-snippets 'latex-mode
                      '(("prp" "\\begin{proposition}\n`%`$0\n\\end{proposition}" "proposition" nil nil nil "/home/waigni/.config/doom/snippets/latex-mode/proposition" nil nil)
                        ("nb" "\\begin{note}\n`%`$0\n\\end{note}" "note" nil nil nil "/home/waigni/.config/doom/snippets/latex-mode/note" nil nil)
-                       ("newlec" "%% -*- TeX-master: \"../main.tex\" ; TeX-engine: xetex -*-\n\\documentclass[../main.tex]{subfile}\n\n\\begin{document}\n$0\n\\end{document}" "newlecturesub" nil nil nil "/home/waigni/.config/doom/snippets/latex-mode/newlecturesub" nil nil)
+                       ("newlec" "%% -*- TeX-master: \"../main.tex\" ; TeX-engine: xetex -*-\n\\documentclass[../main.tex]{subfiles}\n\n\\begin{document}\n$0\n\\end{document}" "newlecturesub" nil nil nil "/home/waigni/.config/doom/snippets/latex-mode/newlecturesub" nil nil)
                        ("newlecmain" "\\subfileinclude{lectures/${1:`(format-time-string \"%Y-%m-%d\")`}}\n$0" "newlecturemain" nil nil nil "/home/waigni/.config/doom/snippets/latex-mode/newlecturemain" nil nil)
                        ("lec" "%% -*- TeX-engine: xetex -*-\n\n\\documentclass[lecture]{twl}\n\n\\title{${1:$$(upcase yas-text)}}\n\\author{${4:Ting Wei Liu}}\n\n\\input{preamble}\n\\usepackage{subfiles}\n\n\\begin{document}\n\\makeatletter\n\\let\\@shorttitle\\@title\n\\RenewDocumentCommand{\\@title}{}{\\@shorttitle{}: ${2:Course Name}}\n\\makeatother\n\n\\maketitle\n\n\\makeatletter\n\\let\\@title\\@shorttitle\n\\makeatother\n\n\\begin{description}\n\\item[Professor] ${6:$$(capitalize yas-text)}${7:\n}\n\\end{description}\n\n\\tableofcontents\n\n$0\n\n\\PrintIndex{}\n\\PrintTheorems{}\n\\end{document}\n" "lecture" nil nil nil "/home/waigni/.config/doom/snippets/latex-mode/lecture" nil nil)
                        ("eg" "\\begin{example}\n`%`$0\n\\end{example}" "example" nil nil nil "/home/waigni/.config/doom/snippets/latex-mode/example" nil nil)
                        ("asg" "%% -*- TeX-engine: xetex -*-\n\n\\documentclass[assignment]{twl}\n\n\\title{${1:$$(upcase yas-text)}: ${2:Assignment} \\texttt{\\#}${3:Number}}\n\\author{${4:twl}}\n\\date{${5:`(format-time-string \"%B %d, %Y\")`}}\n\n\\input{../../preamble}\n\n\\begin{document}\n\\maketitle\n\n\\tableofcontents\n\\clearpage\n\n\\problem{}\n\n$0\n\\end{document}" "assignment" nil nil nil "/home/waigni/.config/doom/snippets/latex-mode/assignment" nil nil)))
 
 
-;;; Do not edit! File generated at Fri May 14 16:15:10 2021
+;;; Do not edit! File generated at Thu May 20 04:10:01 2021
