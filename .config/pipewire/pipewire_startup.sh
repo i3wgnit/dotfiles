54aebb8b1a845ae260a1912d3d9f6061857ef6c6
Ting Wei Liu <i3wgnit@protonmail.com>
2021-09-28 15:39:51 -0400

    Fix pipewire loopbacks

--- .config/pipewire/pipewire_startup.sh
+++ .config/pipewire/pipewire_startup.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-pactl set-default-source "$(pactl get-default-source)"
+SINK="$(pactl get-default-source)"
 
 pw-cli create-node adapter factory.name='support.null-audio-sink' media.class='Audio/Sink' object.linger='true' monitor.channel-volumes='true' node.name='loopback_out' node.description='Loopback_Out'
 pw-cli create-node adapter factory.name='support.null-audio-sink' media.class='Audio/Duplex' object.linger='true' node.name='loopback_mic' node.description='Loopback_Mic'
+pw-metadata 0 default.configured.audio.source '{ "name": "loopback_mic" }'
 
 pactl load-module module-loopback source_dont_move='true' source='loopback_out.monitor'
-pactl load-module module-loopback sink_dont_move='true' source_dont_move='true' sink='loopback_mic'
+pactl load-module module-loopback sink_dont_move='true' source="$SINK" sink='loopback_mic'
+
 pw-link loopback_out:monitor_1 loopback_mic:playback_1
 pw-link loopback_out:monitor_2 loopback_mic:playback_2
-
-pw-metadata 0 default.configured.audio.source '{ "name": "loopback_mic" }'
