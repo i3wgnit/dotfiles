8ecc4250c66d5ed471b8d453f5d9029c7261298a
Ting Wei Liu <i3wgnit@protonmail.com>
2021-12-29 07:07:56 -0500

    Change default source to default mic instead of loopback

--- .config/pipewire/pipewire_startup.sh
+++ .config/pipewire/pipewire_startup.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-SINK="$(pactl get-default-source)"
+SOURCE="$(pactl get-default-source)"
 
 pw-cli create-node adapter factory.name='support.null-audio-sink' media.class='Audio/Sink' object.linger='true' monitor.channel-volumes='true' node.name='loopback_out' node.description='Loopback_Out'
 pw-cli create-node adapter factory.name='support.null-audio-sink' media.class='Audio/Duplex' object.linger='true' node.name='loopback_mic' node.description='Loopback_Mic'
 pactl set-default-source loopback_mic
 
 pactl load-module module-loopback source_dont_move='true' source='loopback_out.monitor'
-pactl load-module module-loopback sink_dont_move='true' source="$SINK" sink='loopback_mic'
+pactl load-module module-loopback sink_dont_move='true' source="$SOURCE" sink='loopback_mic'
+pactl set-default-source "$SOURCE"
 
 pw-link loopback_out:monitor_1 loopback_mic:playback_1
 pw-link loopback_out:monitor_2 loopback_mic:playback_2
