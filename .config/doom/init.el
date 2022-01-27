331bac29fcb2729819f8ffee9fc0e1297f4d9045
Ting Wei Liu <i3wgnit@protonmail.com>
2022-01-27 13:13:13 -0500

    Add grammar module in doom

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -81,7 +81,7 @@
        :checkers
        (syntax +childframe)        ; tasing you for every semicolon you forget
        (spell +aspell +everywhere) ; tasing you for misspelling mispelling
-       ;;grammar                   ; tasing grammar mistake every you make
+       grammar                     ; tasing grammar mistake every you make
 
        :tools
        ;;ansible
